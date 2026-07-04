0f3f77f6608e40c0d9251c77733271d76d3701ff
NazmiIhsan <nazmiderby16@gmail.com>
2026-07-04 11:17:36 +0800

    Fix firebase2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\File;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +18,15 @@ class AppServiceProvider extends ServiceProvider
     {
         if (app()->environment('production')) {
             URL::forceScheme('https');
+        }
+
+        if ($json = env('FIREBASE_CREDENTIALS_JSON')) {
+            File::ensureDirectoryExists(storage_path('app/firebase'));
+
+            File::put(
+                storage_path('app/firebase/chefvirtuo-service-account.json'),
+                $json
+            );
         }
     }
 }
